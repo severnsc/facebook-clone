41c0f90138c821d89721f73d08188bdab43dd308
severnsc <severnsc@gmail.com>
2017-03-24 19:40:40 -0400

    Add update route to friendships

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :index]
-  resources :friendships, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy, :update]
   root 'static_pages#home'
 end
