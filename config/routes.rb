889fbd19225a9bea799afc3ac169e28273ce01c2
severnsc <severnsc@gmail.com>
2017-03-25 22:52:17 -0400

    Users can create posts from users show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :users, only: [:show, :index]
   resources :friendships, only: [:create, :destroy, :update]
+  resources :posts
   root 'static_pages#home'
 end
