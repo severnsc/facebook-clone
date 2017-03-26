12fb1561c703f335979428a18a19380f6f139e7b
severnsc <severnsc@gmail.com>
2017-03-25 22:52:00 -0400

    Add posts controller with RESTful routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to current_user
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "Post edited!"
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.delete
+    flash[:sucess] = "Post deleted!"
+    redirect_to current_user
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
