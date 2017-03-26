e03e8fa9ca561b4e18b4dd0c5aa1a1a33d53e0af
severnsc <severnsc@gmail.com>
2017-03-25 22:51:20 -0400

    Fix posts content column

--- db/migrate/20170326022626_create_posts.rb
+++ db/migrate/20170326022626_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.references :user
-      t.string :content
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20170326022626) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
