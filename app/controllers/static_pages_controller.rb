fa82478ed1649acb679cb1604f94b8bf8fcc6717
severnsc <severnsc@gmail.com>
2017-03-27 15:26:41 -0400

    If logged in, home page is newsfeed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def home
+    if current_user
+      redirect_to posts_path
+    else
+      render 'static_pages/home'
+    end
   end
 end
