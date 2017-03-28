bff1b48af7ab2d41c54f5e8e447872101a05e875
severnsc <severnsc@gmail.com>
2017-03-28 09:51:10 -0400

    Update production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "facebook-clone_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
   host = 'stormy-garden-51940.herokuapp.com/'
   config.action_mailer.default_url_options = {host: host, protocol: 'https'}
 
