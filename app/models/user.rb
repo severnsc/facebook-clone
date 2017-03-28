84e6d90dc12a7d70daba02e6a1e755df09bba728
severnsc <severnsc@gmail.com>
2017-03-28 09:36:04 -0400

    Get omniauth with Facebook to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,14 +26,15 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.first_name = auth.info.first_name   # assuming the user model has a name
-    user.last_name = auth.info.last_name
-    # If you are using confirmable and the provider(s) you use validate emails, 
-    # uncomment the line below to skip the confirmation emails.
-    user.skip_confirmation!
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.password_confirmation = user.password
+      user.first_name = auth.info.name.split(" ").first   # assuming the user model has a name
+      user.last_name = auth.info.name.split(" ").last
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      user.skip_confirmation!
+    end
   end
-end
 end
